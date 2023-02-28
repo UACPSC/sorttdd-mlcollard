dfd79f931eb72709069ccf9e89c880f19bb6efc1
Michael L. Collard <collard@uakron.edu>
2023-02-28 15:58:59 -0500

    Add test for sequence { 2, 1 }

--- sort.cpp
+++ sort.cpp
@@ -7,6 +7,14 @@
 #include "sort.hpp"
 
 // sort in ascending order
-void sort(std::vector<int>& /* v */) {
+void sort(std::vector<int>& v) {
 
+    if (v.size() == 1)
+        return;
+
+    if (v[0] > v[1]) {
+        auto t = v[0];
+        v[0] = v[1];
+        v[1] = t;
+    }
 }
