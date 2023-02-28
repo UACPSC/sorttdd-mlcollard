8133dc048021d242f10532299a2e32c812da2043
Michael L. Collard <collard@uakron.edu>
2023-02-28 16:09:33 -0500

    Add sort for { 3, 2, 1 }

--- sort.cpp
+++ sort.cpp
@@ -6,15 +6,31 @@
 
 #include "sort.hpp"
 
+namespace {
+
+    void swap(int& n1, int& n2) {
+        auto t = n1;
+        n1 = n2;
+        n2 = t;
+    }
+
+    void order(int& n1, int& n2) {
+        if (n1 > n2)
+            swap(n1, n2);
+    }
+}
+
 // sort in ascending order
 void sort(std::vector<int>& v) {
 
     if (v.size() == 1)
         return;
 
-    if (v[0] > v[1]) {
-        auto t = v[0];
-        v[0] = v[1];
-        v[1] = t;
-    }
+    order(v[0], v[1]);
+
+    if (v.size() == 2)
+        return;
+
+    order(v[1], v[2]);
+    order(v[0], v[1]);
 }
